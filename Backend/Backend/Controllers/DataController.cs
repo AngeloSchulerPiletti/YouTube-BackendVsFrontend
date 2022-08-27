12318d2a7f93b9cd3f263327102de96cad670e92
Angelo Schuler Piletti <angelopiletti@gmail.com>
2022-08-27 15:43:47 -0300

    init

---
+++ Backend/Backend/Controllers/DataController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Backend.Controllers
+{
+    [Route("api/")]
+    [ApiController]
+    public class DataController : ControllerBase
+    {
+        [HttpGet]
+        [Route("obter-texto/{numA}/{numB}")]
+        public IActionResult GetData(int numA, int numB)
+        {
+            return Ok(numA * numB);
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,11 @@
+# Backend vs Frontend :mag_right:
+
+:movie_camera: Este repositório contém o código apresentado [neste vídeo do YouTube](https://youtu.be/FX5OUyddURY) por mim!
+
+<br/>
+
+[<img src="./thumb.png"/>](https://youtu.be/FX5OUyddURY)
+
+<br/>
+
+Bons estudos, colega!
